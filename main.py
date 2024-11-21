cfbbf99c743141405ff7aa9504370b5a35bda39c
bruhitsj <lcjay20@gmail.com>
2024-11-21 09:30:10 -0600

    main.py
    My test

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-def Pizza_price(""" I would like to order a pizz""") 
+def pizza_price():
+
+ requested_topping = input("""
+  Write your customer promt here""")
+
 base_price = 15
-toppings_price = 0
-for toppings in requested_toppings:
-  if toppings == "T":
+topping_price = 0
+calculated_topping =[]
+
+for topping in requested_toppimg:
+  continue
+
+
+if topping in calculated_topping
+continue
+
+for topping in requested_topping:
+
+  if topping == "T":
     toppings_price += 1.5
+
+ if topping == "O":
+   toppings_price += 1.25
+
+ if topping == "P":
+   topping_price += 3.5
+
+ if topping == "M":
+   topping_price += 3.75
+
+ if topping == "A":
+    toppings_price += .40
+
+    total_price = base_price + topping_price
+   if total_price > 20:
+     total_price * .95
+
+print(total_price)
+
+total_price = Round(total_price, 2)
+
+print(total_price)
+
+pizza_price ()
